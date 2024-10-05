c68cbb7e68bbf0c7745065003ef1fc08ed24c65a
Jabed Kaium <jabed.kaium@bracits.com>
2024-10-05 20:07:17 +0600

    test added

--- src/test/java/com/example/test/controllers/ControllerUnitTest.java
+++ src/test/java/com/example/test/controllers/ControllerUnitTest.java
@@ -3,6 +3,8 @@ package com.example.test.controllers;
 import com.example.test.Response.BatteryResponse;
 import com.example.test.Response.Statistics;
 import com.example.test.dto.BatteryDto;
+import com.example.test.exception.BatteryNotFoundException;
+import com.example.test.exception.InvalidBatteryListException;
 import com.example.test.models.Battery;
 import com.example.test.services.BatteryService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -24,6 +26,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -93,6 +97,44 @@ public class ControllerUnitTest {
     }
 
     @Test
+    public void invalidBatteryListShouldReturnBadRequest() throws Exception {
+        BatteryDto batteryDto = new BatteryDto();
+        batteryDto.setName("");
+        batteryDto.setPostcode(10000);
+        batteryDto.setWattCapacity(100);
+
+        List<BatteryDto> invalidBatteries = Arrays.asList(batteryDto);
+
+        //mock the behavior of service to throw InvalidBatteryListException
+        doThrow(new InvalidBatteryListException(invalidBatteries)).when(batteryService).saveBatteries(anyList());
+
+        mockMvc.perform(post("/batteries")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(invalidBatteries)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("400"))
+                .andExpect(jsonPath("$.message").value("Invalid input detected."))
+                .andExpect(jsonPath("$.timestamp").exists())
+                .andExpect(jsonPath("$.invalidBatteries").isNotEmpty());
+    }
+
+    @Test
+    public void emptyBatteryListShouldReturnBadRequest() throws Exception {
+        List<Battery> emptyBatteryDtoList = new ArrayList<>();
+
+        //mock the behavior of service to throw illegalArgumentException
+        doThrow(new IllegalArgumentException("Battery list cannot be empty")).when(batteryService).saveBatteries(anyList());
+
+        mockMvc.perform(post("/batteries")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(emptyBatteryDtoList)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("400"))
+                .andExpect(jsonPath("$.message").value("Battery list cannot be empty"))
+                .andExpect(jsonPath("$.timestamp").exists());
+    }
+
+    @Test
     public void getBatteriesByRangeTest() throws Exception {
         String postcodeRange = "10000-20000";
 
@@ -116,5 +158,50 @@ public class ControllerUnitTest {
                         .andExpect(jsonPath("$.response.batteryNames[1]").value("Battery 2"))
                         .andExpect(jsonPath("$.response.statistics.totalWattCapacity").value(250))
                         .andExpect(jsonPath("$.response.statistics.averageWattCapacity").value(125.0));
+    }
+
+    @Test
+    public void emptyPostcodeRangeShouldReturnBadRequest() throws Exception {
+        String postcodeRange = "";
+
+        doThrow(new IllegalArgumentException("Postcode range cannot be empty")).when(batteryService).getBatteryResponse(postcodeRange);
+
+        mockMvc.perform(get("/batteries")
+                .param("postcodeRange", postcodeRange)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("400"))
+                .andExpect(jsonPath("$.message").value("Postcode range cannot be empty"))
+                .andExpect(jsonPath("$.timestamp").exists());
+    }
+
+    @Test
+    public void invalidPostcodeRangeShouldReturnBadRequest() throws Exception {
+        String postcodeRange = "20000-10000";
+
+        doThrow(new IllegalArgumentException("Invalid postcode range")).when(batteryService).getBatteryResponse(postcodeRange);
+
+        mockMvc.perform(get("/batteries")
+                .param("postcodeRange", postcodeRange)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("400"))
+                .andExpect(jsonPath("$.message").value("Invalid postcode range"))
+                .andExpect(jsonPath("$.timestamp").exists());
+    }
+
+    @Test
+    public void shouldReturnBatteryNotFound() throws Exception {
+        String postcodeRange = "30000-40000";
+
+        doThrow(new BatteryNotFoundException("Batteries not found in given postcode range")).when(batteryService).getBatteryResponse(postcodeRange);
+
+        mockMvc.perform(get("/batteries")
+                .param("postcodeRange", postcodeRange)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value("404"))
+                .andExpect(jsonPath("$.message").value("Batteries not found in given postcode range"))
+                .andExpect(jsonPath("$.timestamp").exists());
     }
 }
